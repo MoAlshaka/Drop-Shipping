3dbc9cf2513c778e367e24cbfdf64a6ee46aaf9e
MoAlshaka <mohamedalshaka330@gmail.com>
2024-03-16 23:40:50 +0200

    =handle seller active

---
+++ app/Http/Controllers/Admin/ActiveSellerController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Seller;
+use Illuminate\Http\Request;
+
+class ActiveSellerController extends Controller
+{
+    public function index()
+    {
+        $sellers = Seller::orderBy('id', 'desc')->get();
+        return view('admin.sellers.index', compact('sellers'));
+    }
+    public function active(Request $request, $id)
+    {
+        $seller = Seller::find($id);
+        $seller->update(['is_active' => $request->is_active]);
+
+        return redirect()->route('admin.sellers.index');
+    }
+}
